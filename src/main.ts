89eef0a233013a35f632424756437ec4f8d22877
Pavlos Vinieratos <pvinis@gmail.com>
2021-03-19 11:05:59 +0000

    aha

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,35 @@
 import Fuse from 'fuse.js'
+import low from "lowdb"
+import FileSync from "lowdb/adapters/FileSync"
 
+
+const adapter = new FileSync("db.json")
+const db = low(adapter)
+
+
+
+type Page ={
+	title: string
+	text: string
+	new: boolean
+}
+
+db.defaults({pages: {}}).write()
 
 const list = [
 	"wow"
 ]
 
 
+
+// do i need to add puppeteer here or can i get the text from FF?
+
+
+////
+
+
+
+// add index if it gets slow?
 const fuse = new Fuse(list, {includeScore: true} )
 
 console.log("aaok")
